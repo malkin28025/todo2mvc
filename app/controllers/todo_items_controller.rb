beb06ecb2e9f39f314f6177d2ebd3bb86f1b38d5
Joe Cursi <malkin28025@yahoo.com>
2015-02-15 12:02:19 -0500

    migration change_todo_item

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,12 +1,31 @@
 class TodoItemsController < ApplicationController
+	before_action :find_todo_item, only: [:update, :destory]
 
 	def index
 		@todo_item = TodoItem.new
+		@all_todo_items = TodoItem.all
 	end
 
 	def create
 		@todo_item = TodoItem.create(params.require(:todo_item).permit(:name))
 			redirect_to root_url
 	end
+
+	def update
+		@todo_item = TodoItem.find(params [:id])
+		@todo_item.update(params, require(:todo_item).permit, :complete))
+		redirect_to root_url
+	end
+
+	private
+	def find_todo_item
+		@todo_item = TodoItem.find(params[:id])
+	end
+
+	def redirect_to_root
+		redirect_to_root_url
+	end
+
+
 
 end
